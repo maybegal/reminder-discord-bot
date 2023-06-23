2d568c22e2dab442a3c7320a4e322411c2b6b119
Pinchas <pinhastnt@gmail.com>
2023-06-23 14:02:33 +0300

    working with multiple servers +  added .gitignore

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+// Load config file
 const config = require("./config.json");
 
 const {
   Client,
   GatewayIntentBits,
-  Message,
-  TextChannel,
 } = require("discord.js");
 require("dotenv/config");
 
@@ -16,43 +15,35 @@ const client = new Client({
   ],
 });
 
-// Send random message from the messages list
-function sendRandomMessage() {
-  return config.servers.ofekDigital.messages[
-    Math.floor(Math.random() * config.servers.ofekDigital.messages.length)
-  ];
-}
-
 // Check executed times
 const executedTimes = new Set();
 
+// Main bot code
 client.on("ready", () => {
-  client.user.setActivity(config.activityStatus);
-  console.log("Reminder bot is ready!");
-  setInterval(() => {
+    client.user.setActivity(config.activityStatus);
+    console.log("Reminder bot is ready!");
+
+    setInterval(() => {
     // Get current time
     const currentTime = new Date();
-    // Format current time as "HH:mm"
-    const currentFormattedTime = `${currentTime.getHours()}:${currentTime
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}`;
-    // Check if current time is a reminder times
-    if (
-      config.servers.ofekDigital.reminderTimes.includes(currentFormattedTime) &&
-      !executedTimes.has(currentFormattedTime)
-    ) {
-      // Send message in updates channel
-      client.channels.cache
-        .get(config.servers.ofekDigital.channelId)
-        .send(sendRandomMessage());
 
-      // Reset executed times for next reminder
-      executedTimes.add(currentFormattedTime);
-    }
-  }, 1000);
-  // Big thanks for CodeBlock for helping me to write this bot!
-  // And of course all rights resrved to MaybeGal.
+    // Format current time as "HH:mm"
+    const currentFormattedTime = `${currentTime.getHours()}:${currentTime.getMinutes().toString().padStart(2, "0")}`;
+
+    // Check if current time is a reminder times
+    for (let i = 0; i < config.servers.length; i++) {
+        if (config.servers[i].reminderTimes.includes(currentFormattedTime) && !executedTimes.has(currentFormattedTime)) {
+                // Send message in updates channel
+                client.channels.cache.get(config.servers[i].updateChannel).send(config.servers[i].messages[Math.floor(Math.random() * config.servers[i].messages.length)]);
+
+                // Reset executed times for next reminder
+                executedTimes.add(currentFormattedTime);
+            }
+        }
+    }, 1000);
+    // Big thanks for CodeBlock for helping me to write this bot!
+    // And of course all rights resrved to MaybeGal.
 });
 
+// Login to Discord using bot token
 client.login(process.env.token);
